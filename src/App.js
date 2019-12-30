f72dc125b812a8abb14c4052faf3a63b3c908df1
Andrey Okhotnikov <okhotnikovav@melonfashion.com>
2019-12-30 22:29:07 +0300

    fix video

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ import rita from './images/rita.jpg';
 import nastya30 from './images/nastya30.jpg';
 import vika from './images/vika.jpg';
 
+import mainVideo from './video/Поздравление.mp4';
+
+import iraV from './video/Ире.mp4';
+import olesyaV from './video/Олесе.mp4';
+import nastyaV from './video/Насте.mp4';
+import juliyaV from './video/Насте.mp4';
+import ritaV from './video/Насте.mp4';
+import nastya30V from './video/Насте.mp4';
+import vikaV from './video/Насте.mp4';
+
 import './App.css';
 import data from './data';
 
@@ -18,57 +28,49 @@ function App() {
         <div>
             <h1>Новогоднее обращение предводителя</h1>
             <div className="main-video">
-                <iframe
-                    width="320"
-                    height="320"
-                    src="https://www.youtube.com/embed/IePHmnG9kiE"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen
-                    title="main-video"
-                ></iframe>
+                <video src={mainVideo} controls></video>
             </div>
             <h2>Ищи себя и получай стих в подарок</h2>
             <div className="app">
                 {data.map(item => {
                     let image;
+                    let video;
 
                     switch (item.name) {
                         case 'ira':
                             image = ira;
+                            video = iraV;
                             break;
                         case 'nastya':
                             image = nastya;
+                            video = nastyaV;
                             break;
                         case 'olesya':
                             image = olesya;
+                            video = olesyaV;
                             break;
                         case 'juliya':
                             image = juliya;
+                            video = juliyaV;
                             break;
                         case 'rita':
                             image = rita;
+                            video = ritaV;
                             break;
                         case 'nastya30':
                             image = nastya30;
+                            video = nastya30V;
+
                             break;
                         default:
                             image = vika;
+                            video = vikaV;
                     }
 
                     return (
                         <div className="item">
                             {active === item.name ? (
-                                <iframe
-                                    width="320"
-                                    height="320"
-                                    src={item.video}
-                                    frameborder="0"
-                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                                    allowfullscreen
-                                    key={item.name}
-                                    title={item.name}
-                                ></iframe>
+                                <video src={video} controls autoPlay></video>
                             ) : (
                                 <img
                                     src={image}
