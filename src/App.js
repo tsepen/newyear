d80a461c4bde447fd264e4e38784c9cde54831e7
Andrey Okhotnikov <okhotnikovav@melonfashion.com>
2019-12-30 14:22:58 +0300

    create template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ira from './images/cat.jpeg';
 import './App.css';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [active, setActive] = useState('');
+
+    return (
+        <div>
+            <h1>Новогоднее обращение предводителя</h1>
+            <div className="main-video">
+                <iframe
+                    width="320"
+                    height="320"
+                    src="https://www.youtube.com/embed/hY7m5jjJ9mM"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                    title="main-video"
+                ></iframe>
+            </div>
+            <h2>Ищи себя и получай стих в подарок</h2>
+            <div className="app">
+                {data.map(item => {
+                    return (
+                        <div className="item">
+                            {active === item.name ? (
+                                <iframe
+                                    width="320"
+                                    height="320"
+                                    src={item.video}
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen
+                                    key={item.name}
+                                    title={item.name}
+                                ></iframe>
+                            ) : (
+                                <img
+                                    src={ira}
+                                    className="App-logo"
+                                    alt="logo"
+                                    onClick={() => setActive(item.name)}
+                                />
+                            )}{' '}
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        name: 'ira',
+        video: 'https://www.youtube.com/embed/hY7m5jjJ9mM',
+    },
+    {
+        name: 'nastya',
+        video: 'https://www.youtube.com/embed/hY7m5jjJ9mM',
+    },
+    {
+        name: 'olesya',
+        video: 'https://www.youtube.com/embed/hY7m5jjJ9mM',
+    },
+    {
+        name: 'juliya',
+        video: 'https://www.youtube.com/embed/hY7m5jjJ9mM',
+    },
+    {
+        name: 'rita',
+        video: 'https://www.youtube.com/embed/hY7m5jjJ9mM',
+    },
+    {
+        name: 'nastya30',
+        video: 'https://www.youtube.com/embed/hY7m5jjJ9mM',
+    },
+    {
+        name: 'vika',
+        video: 'https://www.youtube.com/embed/hY7m5jjJ9mM',
+    },
+];
