b99b98e3112404ac077200756aa25ff75d164227
Andrey Okhotnikov <okhotnikovav@melonfashion.com>
2019-12-31 09:33:44 +0300

    add video

--- src/App.js
+++ src/App.js
@@ -13,11 +13,10 @@ import mainVideo from './video/Поздравление.mp4';
 import iraV from './video/Ире.mp4';
 import nastyaV from './video/Насте.mp4';
 import olesyaV from './video/Олесе.mp4';
-
-import juliyaV from './video/Насте.mp4';
-import ritaV from './video/Насте.mp4';
-import nastya30V from './video/Насте.mp4';
-import vikaV from './video/Насте.mp4';
+import juliyaV from './video/Юле.mp4';
+import ritaV from './video/Рите.mp4';
+import nastya30V from './video/Насте30.mp4';
+import vikaV from './video/Вике.mp4';
 
 import './App.css';
 import data from './data';
@@ -29,9 +28,11 @@ function App() {
         <div>
             <h1>Новогоднее обращение предводителя</h1>
             <div className="main-video">
-                <video src={mainVideo} controls></video>
+                <video src={mainVideo} controls muted playsinline loop>
+                    {' '}
+                </video>
             </div>
-            <h2>Ищи себя и получай стих в подарок</h2>
+            <h2>Для тебя есть персональное поздравление</h2>
             <div className="app">
                 {data.map(item => {
                     let image;
@@ -69,13 +70,23 @@ function App() {
                     }
 
                     return (
-                        <div className="item">
-                            <video
-                                src={video}
-                                controls
-                                // autoPlay
-                                poster={image}
-                            ></video>
+                        <div className="item" key={item.name}>
+                            {active === item.name ? (
+                                <video
+                                    src={video}
+                                    controls
+                                    autoPlay
+                                    muted
+                                    playsinline
+                                    loop
+                                ></video>
+                            ) : (
+                                <img
+                                    src={image}
+                                    alt={item.name}
+                                    onClick={() => setActive(item.name)}
+                                />
+                            )}
                         </div>
                     );
                 })}
