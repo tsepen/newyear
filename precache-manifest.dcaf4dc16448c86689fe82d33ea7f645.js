f8c5e3822e1e4988111f9adaec0d1e7e8892919d
Andrey Okhotnikov <okhotnikovav@melonfashion.com>
2019-12-31 09:34:44 +0300

    Updates

---
+++ precache-manifest.dcaf4dc16448c86689fe82d33ea7f645.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfacfff6f5a4ca5369b85c97b6b05acd",
+    "url": "/newyear/index.html"
+  },
+  {
+    "revision": "c077b3c0f22f83d6a513",
+    "url": "/newyear/static/css/main.6f924aa9.chunk.css"
+  },
+  {
+    "revision": "83602f4a69290c76942b",
+    "url": "/newyear/static/js/2.50ac958b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newyear/static/js/2.50ac958b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c077b3c0f22f83d6a513",
+    "url": "/newyear/static/js/main.3a368b91.chunk.js"
+  },
+  {
+    "revision": "142745484fe078f7e2a4",
+    "url": "/newyear/static/js/runtime-main.8114ab7b.js"
+  },
+  {
+    "revision": "2d69fd016badabc3500f267e9508aa99",
+    "url": "/newyear/static/media/ira.2d69fd01.jpg"
+  },
+  {
+    "revision": "e7a7d252629f033707d38e7a7c53682e",
+    "url": "/newyear/static/media/julia.e7a7d252.jpg"
+  },
+  {
+    "revision": "f6607762062875acd7c09ffe07d8e55a",
+    "url": "/newyear/static/media/nastya.f6607762.jpg"
+  },
+  {
+    "revision": "5e9dc1373365080c5f185e04869c23c6",
+    "url": "/newyear/static/media/nastya30.5e9dc137.jpg"
+  },
+  {
+    "revision": "30ad2ffa36334f238da8cc3bf859a68f",
+    "url": "/newyear/static/media/olesya.30ad2ffa.jpg"
+  },
+  {
+    "revision": "8e9a27ca0f8d6e41bb0a0addc534e0ca",
+    "url": "/newyear/static/media/rita.8e9a27ca.jpg"
+  },
+  {
+    "revision": "3dbc419d40e2bb9a31632b706406206c",
+    "url": "/newyear/static/media/vika.3dbc419d.jpg"
+  },
+  {
+    "revision": "25ba13807a224ab534376f4659319b81",
+    "url": "/newyear/static/media/Вике.25ba1380.mp4"
+  },
+  {
+    "revision": "1a9e4b64f3c6e50b546ec0fa01685844",
+    "url": "/newyear/static/media/Ире.1a9e4b64.mp4"
+  },
+  {
+    "revision": "229089d9903eaab1f4edf138db40cede",
+    "url": "/newyear/static/media/Насте.229089d9.mp4"
+  },
+  {
+    "revision": "2aefcfb8886c451d36585c09e66cf59e",
+    "url": "/newyear/static/media/Насте30.2aefcfb8.mp4"
+  },
+  {
+    "revision": "1f41d399d97d8f4d31ca862288e50106",
+    "url": "/newyear/static/media/Олесе.1f41d399.mp4"
+  },
+  {
+    "revision": "919e18473f56aa95f8c5c1aef1cb529d",
+    "url": "/newyear/static/media/Поздравление.919e1847.mp4"
+  },
+  {
+    "revision": "fe6d632c5d172d7e54150070ccdb7f19",
+    "url": "/newyear/static/media/Рите.fe6d632c.mp4"
+  },
+  {
+    "revision": "fdf989a766cac265c1a7d2ebbf64329e",
+    "url": "/newyear/static/media/Юле.fdf989a7.mp4"
+  }
+]);
