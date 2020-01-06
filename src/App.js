65cf071fca8d563a630af27fe49e336331f1f602
Andrey Okhotnikov <okhotnikovav@melonfashion.com>
2020-01-06 17:23:58 +0300

    add password

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React, { useState } from 'react';
-
-import ira from './images/ira.jpg';
-import nastya from './images/nastya.jpg';
-import olesya from './images/olesya.jpg';
-import juliya from './images/julia.jpg';
-import rita from './images/rita.jpg';
-import nastya30 from './images/nastya30.jpg';
-import vika from './images/vika.jpg';
-import den from './images/den.jpg';
-
-import mainVideo from './video/Поздравление.mp4';
-
-import iraV from './video/Ире.mp4';
-import nastyaV from './video/Насте2.mp4';
-import olesyaV from './video/Олесе2.mp4';
-import juliyaV from './video/Юле.mp4';
-import ritaV from './video/Рите.mp4';
-import nastya30V from './video/Насте30.mp4';
-import vikaV from './video/Вике.mp4';
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import data from './data';
+import den from './images/den.jpg';
+import ira from './images/ira.jpg';
+import juliya from './images/julia.jpg';
+import nastya from './images/nastya.jpg';
+import nastya30 from './images/nastya30.jpg';
+import olesya from './images/olesya.jpg';
+import rita from './images/rita.jpg';
+import vika from './images/vika.jpg';
+import vikaV from './video/Вике.mp4';
+import iraV from './video/Ире.mp4';
+import nastyaV from './video/Насте2.mp4';
+import nastya30V from './video/Насте30.mp4';
+import olesyaV from './video/Олесе2.mp4';
+import mainVideo from './video/Поздравление.mp4';
+import ritaV from './video/Рите.mp4';
+import juliyaV from './video/Юле.mp4';
 
 function App() {
     const [active, setActive] = useState('');
+    const [isLogin, setIsLogin] = useState(false);
+
+    useEffect(() => {
+        const isLogin = localStorage.getItem('isLogin');
+
+        if (isLogin) {
+            setIsLogin(true);
+        }
+    }, []);
+
+    const onPasswordChenge = value => {
+        if (value.toLowerCase() === 'слон') {
+            setIsLogin(true);
+            localStorage.setItem('isLogin', true);
+        }
+    };
+
+    if (!isLogin) {
+        return (
+            <>
+                <h1>Введи секретное слово (его знает Настя-акробатка)</h1>
+                <input
+                    placeholder="слово"
+                    onChange={e => onPasswordChenge(e.target.value)}
+                />
+            </>
+        );
+    }
 
     return (
         <div>
